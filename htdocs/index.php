713918be14dc01352ee8bcc79ede84097c190972
Berandal | OWL SQUAD <33419193+berandal666@users.noreply.github.com>
2017-11-06 18:24:39 +0700

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>iBerandal!</title>
+  <meta content="Blablablaa" name="description">
+  <meta content="Blablablaa" name="keywords">
+  <meta content="2 days" name="revisit-after">
+  <meta content="index,follow" name="robots">
+  <meta charset="UTF-8">
+  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+  <link href='http://www.rw-designer.com/icon-view/6935.png' rel='icon' type='image/icon'>
+  <script src="js/jquery.js" type="text/javascript">
+  </script>
+  <script src="js/queryLoader.js" type="text/javascript">
+  </script>
+  <script src="js/scripts.js" type="text/javascript">
+  </script>
+</head>
+<body>
+  <link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+  <div class="wrapper">
+    <div class="container">
+      <div id="logo">
+        <img class="muter" height="450" src="os.png" width="450">
+        <p>BERANDAL</p>
+      </div>
+      <div class="table">
+        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%">
+          <tr>
+            <td align="center">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" width="500">
+                <tr>
+                  <td align="center" width="30%">
+                    <div class="btn btn-5">
+                      <a href="/os">MAIN</a>
+                    </div>
+                  </td>
+                  <td align="center" width="30%">
+                    <div class="btn btn-5">
+                      <a href="/tools">Tools</a>
+                    </div>
+                  </td>
+                  <td align="center" width="30%">
+                    <div class="btn btn-5">
+                      <a href="/belajar">Belajar</a>
+                    </div>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+  <script type='text/javascript'>
+         QueryLoader.init();
+  </script> 
+  <script type="text/javascript">
+         if (typeof document.onselectstart != "undefined") {
+             document.onselectstart = new Function("return false");
+         } else {
+             document.onmousedown = new Function("return false");
+             document.onmouseup = new Function("return true");
+         }
+  </script>
+</body>
+</html>
